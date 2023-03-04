0ad6490ed245522964e0fdb8b77f5fbc2f63b334
Kerem Baydogan <kerembaydogan@gmail.com>
2023-03-04 03:38:42 +0300

    no message

--- Assets/AsmdefHelper/CompileLocker/Editor/CompileLocker.cs
+++ Assets/AsmdefHelper/CompileLocker/Editor/CompileLocker.cs
@@ -1,26 +1,28 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
-/// <summary>
-/// original from: https://gist.github.com/decoc/bde047ac7ad8c9bfce7eb408f2712424
-/// This editor utility can lock/unlock unity script compile from menu item.
-/// See more https://raspberly.hateblo.jp/entry/InvalidateUnityCompile
-/// </summary>
-namespace AsmdefHelper.CompileLocker.Editor {
-    public static class CompileLocker {
-        const string menuPath = "AsmdefHelper/Compile Lock";
+namespace AsmdefHelper.CompileLocker.Editor
+{
+    public static class CompileLocker
+    {
+        private const string MenuPath = "AsmdefHelper/Compile Lock";
 
-        [MenuItem(menuPath, false, 1)]
-        static void Lock() {
-            var isLocked = Menu.GetChecked(menuPath);
-            if (isLocked) {
+
+        [MenuItem(MenuPath, false, 1)]
+        private static void Lock()
+        {
+            var isLocked = Menu.GetChecked(MenuPath);
+            if (isLocked)
+            {
                 Debug.Log("Compile Unlocked");
                 EditorApplication.UnlockReloadAssemblies();
-                Menu.SetChecked(menuPath, false);
-            } else {
+                Menu.SetChecked(MenuPath, false);
+            }
+            else
+            {
                 Debug.Log("Compile Locked");
                 EditorApplication.LockReloadAssemblies();
-                Menu.SetChecked(menuPath, true);
+                Menu.SetChecked(MenuPath, true);
             }
         }
     }
